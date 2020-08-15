45fa352a65e67427b0fd7ce1c838f63fc12dc559
Kordell Parrish <parrishkordell@gmail.com>
2020-08-15 04:51:37 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,6 +28,10 @@ class String
   end
 
   def count_sentences
-   self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    count = []
+    count << self.split(".")
+    count << self.split("!")
+    count << self.split("?")
+    count.delete_if 
   end
 end
